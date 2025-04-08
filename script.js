ff3e67576467316a43354365b7de3f5d0278c985
sofiaz8 <sofiaz700000@gmail.com>
2025-04-08 13:35:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector(".contact form");
 
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("Iesiegt", function(event) {
         event.preventDefault();
         alert("Thank you for contacting us! We will get back to you shortly.");
     });
